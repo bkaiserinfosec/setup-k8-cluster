40f31052e8140139262164606e3b162b5a4dce7b
BKaiserJBFG <brian@jbfinegoods.com>
2021-01-03 23:49:59 -0800

    Initial commit

---
+++ kubernetes-worker-centos8-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Step 1 - Pre-Configure the System
+dnf -y upgrade
+setenforce 0
+sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+modprobe br_netfilter
+firewall-cmd --add-masquerade --permanent
+firewall-cmd --reload
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+swapoff -a
+# Step 2 - Install Docker CE
+dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+dnf -y install https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+dnf install docker-ce --nobest -y
+systemctl start docker
+systemctl enable docker
+# Step 3 - Install Kubernetes
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+dnf upgrade -y
+dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+systemctl enable kubelet
+systemctl start kubelet
+# Step 4 - Configure Kubernetes
+firewall-cmd --zone=public --permanent --add-port={10250,30000-32767}/tcp
+firewall-cmd --reload
+# Step 5 - Join the Cluster
+kubeadm join $1:6443 --token $2 \
+    --discovery-token-ca-cert-hash $3
